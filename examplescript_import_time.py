079c69ca80df1bf3330b5368298ba0d9ad263a52
Todor Todorov <ttodorov@programmer.net>
2019-07-31 20:04:55 +0300

    example script import time

--- examplescript_import_time.py
+++ examplescript_import_time.py
@@ -1,1 +1,27 @@
-TODO
+#!/bin/env python3
+"""
+Example script for dynamic loading during import time
+Play with the imports and rerun the script
+"""
+
+# Play with the imports -------------------------------------------------------
+# (comment and uncomment some of them and rerun the script)
+# from examplepackage import examplemodule
+# from examplepackage import exampleclass
+# from examplepackage import examplefunction
+# -----------------------------------------------------------------------------
+
+import sys
+from pprint import pprint
+import examplepackage
+
+print("Current import status:")
+print("======================")
+pprint(examplepackage.REGISTRY)
+print()
+print()
+print("Current imported modules")
+print("========================")
+for module in [m for m in sys.modules if m.startswith('example')]:
+    print(module, ':', sys.modules[module])
+print()
