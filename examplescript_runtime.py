50442626368a571c1c6bb3e91c6caa628463f499
Todor Todorov <ttodorov@programmer.net>
2019-07-31 20:32:46 +0300

    example script runtime

--- examplescript_runtime.py
+++ examplescript_runtime.py
@@ -1,1 +1,32 @@
-TODO
+#!/bin/env python3
+"""
+Example script for dynamic loading during runtime
+"""
+# import sys
+from pprint import pprint
+import examplepackage
+
+print("!!!")
+print("^C for exit")
+print("!!!")
+print()
+print("Initial import status:")
+print("======================")
+pprint(examplepackage.REGISTRY)
+print()
+while True:
+    print("You can dinamically import some of these:")
+    print("=========================================")
+    print(examplepackage.REGISTRY.keys())
+    print()
+    toimport = input("Type one name to dynamically import: ")
+    print()
+    if not examplepackage.REGISTRY.get(toimport):
+        print("Check your spelling and try again!")
+        print()
+        continue
+    exec(f'examplepackage.{toimport}')
+    print("Current import status:")
+    print("======================")
+    pprint(examplepackage.REGISTRY)
+    print()
